30ac98e65be5cef4a74d5cebbf6403976516b249
Wesley Tavares <wesleyTavaresDev@outlook.com>
2023-02-02 15:01:37 -0300

    telescope-fzf-native setuped

--- lua/lsp-config/language_servers.lua
+++ lua/lsp-config/language_servers.lua
@@ -1,25 +1,26 @@
 require('mason').setup()
+require('neodev').setup()
 
 local lspconfig = require('lspconfig')
 local lsp_defaults = lspconfig.util.default_config
 
 lsp_defaults.capabilities = vim.tbl_deep_extend(
-  'force',
-  lsp_defaults.capabilities,
-  require('cmp_nvim_lsp').default_capabilities()
+	'force',
+	lsp_defaults.capabilities,
+	require('cmp_nvim_lsp').default_capabilities()
 )
 
 
 require("mason-lspconfig").setup_handlers {
-        -- The first entry (without a key) will be the default handler
-        -- and will be called for each installed server that doesn't have
-        -- a dedicated handler.
-        function (server_name) -- default handler (optional)
-            require("lspconfig")[server_name].setup {}
-        end,
-        -- Next, you can provide a dedicated handler for specific servers.
-        -- For example, a handler override for the `rust_analyzer`:
-    }
+	-- The first entry (without a key) will be the default handler
+	-- and will be called for each installed server that doesn't have
+	-- a dedicated handler.
+	function(server_name) -- default handler (optional)
+		require("lspconfig")[server_name].setup {}
+	end,
+	-- Next, you can provide a dedicated handler for specific servers.
+	-- For example, a handler override for the `rust_analyzer`:
+}
 
 
 --[[lspconfig.sumneko_lua.setup({
@@ -28,7 +29,3 @@ require("mason-lspconfig").setup_handlers {
     debounce_text_changes = 150,
   },
 })--]]
-
-
-
-

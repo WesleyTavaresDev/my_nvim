906357ac8ae8d83f4b335fa71d31d12f1662a7a8
Wesley Tavares <wesleyTavaresDev@outlook.com>
2023-02-09 03:24:43 -0300

    blank lines removed

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -1,89 +1,87 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function()
-	local use = use;
+			local use = use;
 
-	use 'wbthomason/packer.nvim'
-	use 'Mofiqul/dracula.nvim'
-	use "rebelot/kanagawa.nvim"
-	use 'RRethy/nvim-base16'
-	use 'romgrk/barbar.nvim'
-	use 'nvim-lualine/lualine.nvim'
-	use 'nvim-tree/nvim-web-devicons'
-	use { 'michaelb/sniprun', run = 'bash ./install.sh' }
-	use 'f-person/git-blame.nvim'
-	use 'cdelledonne/vim-cmake'
-	use 'voldikss/vim-floaterm'
-	use "lukas-reineke/indent-blankline.nvim"
+			use 'wbthomason/packer.nvim'
+			use 'Mofiqul/dracula.nvim'
+			use "rebelot/kanagawa.nvim"
+			use 'RRethy/nvim-base16'
+			use 'romgrk/barbar.nvim'
+			use 'nvim-lualine/lualine.nvim'
+			use 'nvim-tree/nvim-web-devicons'
+			use { 'michaelb/sniprun', run = 'bash ./install.sh' }
+			use 'f-person/git-blame.nvim'
+			use 'cdelledonne/vim-cmake'
+			use 'voldikss/vim-floaterm'
 
-	use {
-		"windwp/nvim-autopairs",
-		config = function() require("nvim-autopairs").setup {} end
-	}
+			use {
+					"windwp/nvim-autopairs",
+					config = function() require("nvim-autopairs").setup {} end
+			}
 
-	use { "L3MON4D3/LuaSnip", tag = "v<CurrentMajor>.*", run = " make install_jsregexp" }
+			use { "L3MON4D3/LuaSnip", tag = "v<CurrentMajor>.*", run = " make install_jsregexp" }
 
-	--Autocompletition_nvim_CPM
-	use {
-		'hrsh7th/nvim-cmp',
-		requires = {
-			'hrsh7th/cmp-nvim-lsp',
-			'hrsh7th/cmp-buffer',
-			'hrsh7th/cmp-path',
-			'hrsh7th/cmp-cmdLine',
-			'saadparwaiz1/cmp_luasnip',
-			'hrsh7th/cmp-nvim-lsp-document-symbol',
-			'hrsh7th/cmp-nvim-lsp-signature-help',
-			'petertriho/cmp-git',
-			'onsails/lspkind-nvim',
-			'chrisgrieser/cmp-nerdfont',
-		},
-	}
+			--Autocompletition_nvim_CPM
+			use {
+					'hrsh7th/nvim-cmp',
+					requires = {
+							'hrsh7th/cmp-nvim-lsp',
+							'hrsh7th/cmp-buffer',
+							'hrsh7th/cmp-path',
+							'hrsh7th/cmp-cmdLine',
+							'saadparwaiz1/cmp_luasnip',
+							'hrsh7th/cmp-nvim-lsp-document-symbol',
+							'hrsh7th/cmp-nvim-lsp-signature-help',
+							'petertriho/cmp-git',
+							'onsails/lspkind-nvim',
+							'chrisgrieser/cmp-nerdfont',
+					},
+			}
 
-	--Mason and LSP
-	use {
-		'neovim/nvim-lspconfig',
-		'williamboman/mason.nvim',
-		'williamboman/mason-lspconfig.nvim',
-		'j-hui/fidget.nvim',
-		'folke/neodev.nvim',
-		'jose-elias-alvarez/null-ls.nvim',
-	}
+			--Mason and LSP
+			use {
+					'neovim/nvim-lspconfig',
+					'williamboman/mason.nvim',
+					'williamboman/mason-lspconfig.nvim',
+					'j-hui/fidget.nvim',
+					'folke/neodev.nvim',
+					'jose-elias-alvarez/null-ls.nvim',
+			}
 
-	--Telescope
-	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.x',
-		requires = { { 'nvim-lua/plenary.nvim' },
-			{ 'nvim-telescope/telescope-fzf-native.nvim',
-				run = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' },
-		},
-	}
+			--Telescope
+			use {
+					'nvim-telescope/telescope.nvim', tag = '0.1.x',
+					requires = { { 'nvim-lua/plenary.nvim' },
+							{ 'nvim-telescope/telescope-fzf-native.nvim',
+									run = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' },
+					},
+			}
 
-	--Treesitter
-	use {
-		'nvim-treesitter/nvim-treesitter',
-		run = function()
-			local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
-			ts_update()
-		end,
-	}
-	--NeoVim Tree
-	use {
-		'nvim-tree/nvim-tree.lua',
-		tag = 'nightly'
-	}
+			--Treesitter
+			use {
+					'nvim-treesitter/nvim-treesitter',
+					run = function()
+						local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+						ts_update()
+					end,
+			}
+			--NeoVim Tree
+			use {
+					'nvim-tree/nvim-tree.lua',
+					tag = 'nightly'
+			}
 
-	--navigation;
-	use {
-		'numToStr/Navigator.nvim',
-		config = function()
-			require('Navigator').setup()
-		end
-	}
-	--tmux
-	use({
-		"aserowy/tmux.nvim",
-		config = function() return require("tmux").setup() end
-	})
-
-end)
+			--navigation;
+			use {
+					'numToStr/Navigator.nvim',
+					config = function()
+						require('Navigator').setup()
+					end
+			}
+			--tmux
+			use({
+					"aserowy/tmux.nvim",
+					config = function() return require("tmux").setup() end
+			})
+		end)
